ae96d1ffcf2d0abcf23f61f07436a1df7926b313
Madeline Stevens <stevensm1987@gmail.com>
2017-06-18 19:09:10 -0700

    added nav bar html links

--- cookie.js
+++ cookie.js
@@ -1,5 +1,4 @@
-
-'use stict';
+'use strict';
 
 //global variables
 //global variable store hours
@@ -9,10 +8,7 @@ var samStore = [];
 // declaring a variable called shopTable and rendering to the DOM using the id of shops and we'll be using this later which is why it needs to be global
 var shopTable = document.getElementById('shops');
 
-//sam added this
-//var theForm...
-
-//CONSTRUCTOR FUNCTION (that is technically an object) aka the maker of the stores (with properties)
+//CONSTRUCTOR FUNCTION
 function Store(location, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerSale) {
   //properties that will become instances on the store objects to come
   this.location = location;
@@ -23,13 +19,31 @@ function Store(location, minCustomersPerHour, maxCustomersPerHour, avgCookiesPer
   this.hourlyCust = [];
   this.hourlySales = [];
   this.totalSales = 0; //total daily sales per hour starts at 0 then counts up
+  this.calcCustomersPerHour();
   this.getCookiesSold();
-  samStore.push(this); //pushing everthing into the samStore array
+  samStore.push(this);
   this.render();//added this to prove that if we render up top my table data shows up
 };
 
+//rendering to the DOM
+Store.prototype.render = function() {
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.location;
+  trEl.appendChild (tdEl); //appending the data to the table row
+  for (var i = 0; i < hours.length; i++) { //should this be hourlySales??
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.hourlySales[i];
+    trEl.appendChild(tdEl);
+  };
+  var tdTotal = document.createElement('td');
+  tdTotal.textContent = this.totalSales;
+  trEl.appendChild(tdTotal);
+  shopTable.appendChild(trEl);
+};
+
 //methods
-//method #1 getting an random number of people per hour. Defining the behaviors of our instances. The prototype is a property of the constructor. The objects that the constructor is making won't inherit these methods.
+//method #1 getting an random number of people per hour. Defining the behaviors of our instances.
 Store.prototype.calcCustomersPerHour = function() {
   for(var i = 0; i < hours.length; i++){
     var cust = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour) + this.minCustomersPerHour);
@@ -38,41 +52,22 @@ Store.prototype.calcCustomersPerHour = function() {
 };
 //method #2 calculating cookie sales per hour now that we've generated the random number of Cx per hour
 Store.prototype.getCookiesSold = function () {
-  this.calcCustomersPerHour(); //calling the method that calcs the random Cx/hour and now that we know it...we can work on this current method that uses it to calculate cookie sales
+  this.calcCustomersPerHour();
   for (var i = 0; i < hours.length; i++) {
     var cookies = Math.ceil(this.hourlyCust[i] * this.avgCookiesPerSale);
     this.hourlySales.push(cookies);
-    this.totalSales += cookies; //+-hourlysales becuase we're adding the hourly sales to the total sales which starts at 0.
+    this.totalSales += cookies;
   }
 };
 
-//****************************************
-//functions to create (or render) the table
-//create the ???
-Store.prototype.render = function() {
-  var trEl = document.createElement ('tr');
-  var tdEl = document.createElement ('td'); //these are the locations
-  tdEl.textContent = this.location; //these are the locations
-  trEl.appendChild (tdEl); //appending the data to the table row
-  for (var i = 0; i < hours.length; i++) { //adding +1 so that it adds my last array in the last colomn
-    var tdEl = document.createElement ('td');
-    //give it content
-    tdEl.textContent = this.hourlySales[i];
-    trEl.appendChild(tdEl);
-  }
-  tdEl = document.createElement ('td');
-  tdEl.textContent = this.totalSales;
-  // console.log (this.totalSales); Don't need???
-  trEl.appendChild(tdEl);
-  shopTable.appendChild(trEl);
-};
+//this is where the render function WAS
 
 //create header row method
 function createHeaderRow() {
   var headrow = document.createElement('tr');
   var tableHeader = document.createElement('th');
   tableHeader.textContent = 'Stores';
-  headrow.appendChild(tableHeader);
+  headrow.appendChild(tableHeader);//headrow.appendChild(tableHeader);
   for (var i = 0; i < hours.length; i++) {
     var tableHeader = document.createElement('th');
     tableHeader.textContent = hours[i];
@@ -85,49 +80,54 @@ function createHeaderRow() {
 }
 // start of render all function
 function renderAll(){
-  console.log('stores......', samStore);
-  for(var x = 0; x < samStore.length; x++){
-    console.log('HELLO');
-    samStore[x].render();
+  for(var i = 0; i < samStore.length; i++) {
+    samStore[i].render();
   }
-}
-//start of creating footer row function with total of totals cell
+};
+//start of creating footer row function (with total of totals cell)
 function makeFooterRow() {
   var footrow = document.createElement('tr');
   var tableFooter = document.createElement('th');
   tableFooter.textContent = 'totals per hour';
   footrow.appendChild(tableFooter);
-
-  grandTotal = [];
-  // }
+  var grandTotal = []; //added var????
   for (var i = 0; i < hours.length; i++) {
     var totalEachHour = 0;
     for (var j = 0; j < samStore.length; j++) {
       totalEachHour += samStore[j].hourlySales[i];
     }
-    tableFooter = document.createElement('th');//getting data but this does something with it
+    tableFooter = document.createElement('th');//repeating what I've already written up top?
     tableFooter.textContent = totalEachHour;
-    footrow.appendChild(tableFooter);
+    footrow.appendChild(tableFooter);//???
     grandTotal.push(totalEachHour);
   }
-  tableFooter = document.createElement ('th');
+  tableFooter = document.createElement('th');
   tableFooter.textContent = grandTotal.reduce(function(total,sum){
     return total + sum;
-  });//running an IIFE (immediately invoked function expression) which is an immediately called, nameless function within a
-  footrow.appendChild (tableFooter);
-  shopTable.appendChild(footrow);
+  });
+  footrow.appendChild(tableFooter);
+  shopTable.appendChild(footrow);//THERE WAS A SPACE HERE
 }
 //end of footer row function
 
-//    ...need to add parseInt after equals
+//these are going to create the instances for each store.
+new Store('Pike Place Market', 23, 65, 6.5);
+new Store('SeaTac Airport', 3, 24, 1.2);
+new Store('Seattle Center', 11, 38, 3.7);
+new Store('Capitol Hill', 20, 38, 2.3);
+new Store('Alki', 3, 24, 1.2);
+
+//event Handler
 function handleSubmission(event){
   event.preventDefault();
   var localInput = event.target.loca.value;
   var minInput = parseInt(event.target.minCust.value);
   var maxInput = parseInt(event.target.maxCust.value);
   var avgCookies = parseFloat(event.target.avgC.value);
-
-//changed this to var newStore= new CookieStand (loc, min, max, avg)
+  if (!event.target.loca.value || !event.target.minCust.value || !event.target.maxCust.value || !event.target.avgC.value) {
+    return alert ('Fields cannot be empty.');
+  }
+// calling new Store with arguments?
   new Store (localInput, minInput, maxInput, avgCookies);
 //this will clear the text field boxes
   event.target.loca.value = null;
@@ -135,6 +135,37 @@ function handleSubmission(event){
   event.target.maxCust.value = null;
   event.target.avgC.value = null;
   shopTable.innerHTML = '';
+
+  var storeExists = doesStoreExist (storeLocations, name);
+  var index;
+
+  function doesStoreExist (x, y) {
+    for (var i = 0; i < x.length; i++) {
+      if (x[i].name === y) {
+        index = i;
+        return true;
+      } else {
+        return false;
+      }
+      doesStoreExist (storeLocation, name);
+
+      if (storeExists === true) {
+        console.log ('found name');
+        samStore[index].minCust = parseInt(minCust);
+        samStore[index].maxCust = parseInt(maxCust);
+        samStore[index].avgC = parseFloat(avgC);
+        samStore[index].hourlyCust = [];
+        samStore[index].hourlySales = [];
+        samStore[index].totalSales = 0;
+        samStore[index].calcCustomersPerHour();
+        samStore[index].getCookiesSold();
+      }
+
+      if (storeExists === false)
+        var newStore = new Store(localInput, minInput, maxInput, avgCookies);
+    }
+  }
+
   createHeaderRow();
   renderAll();
   makeFooterRow();
@@ -144,11 +175,6 @@ createHeaderRow();
 renderAll();
 makeFooterRow();
 
-//these are going to create the instances for each store.
-new Store('Pike Place Market', 23, 65, 6.5);
-new Store('SeaTac Airport', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Alki', 3, 24, 1.2);
+//instances used to be here
 
 newLocations.addEventListener('submit', handleSubmission);
